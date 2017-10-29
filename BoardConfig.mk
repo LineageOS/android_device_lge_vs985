a3fae841364765a4c96d25ee39f8c039d8d8043a
ZjemCiKolege <frager1911@gmail.com>
2017-10-29 14:04:16 +0100

    vs985: bt: add vnd_g3.txt
    Change-Id: Iea6f65a46db23ec1eadcbd903a708f11c68f6db6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,7 @@ TARGET_BOARD_INFO_FILE ?= device/lge/vs985/board-info.txt
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/vs985/bluetooth
+BOARD_CUSTOM_BT_CONFIG := device/lge/vs985/bluetooth/vnd_g3.txt
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_vs985_defconfig
